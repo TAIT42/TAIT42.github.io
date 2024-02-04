cb215b02a5099b10c1b92ab71bc200f7f2d6853f
TAIT42 <124452244+TAIT42@users.noreply.github.com>
2024-02-04 01:01:06 -0600

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "T9st06h5mXf9TLsTrcV9GdjwtL1c8fJ04RJRuO3KPdvoYwSPC5sMkoC1Z5zDkzP3";
 /**
  * Get list of teams in event
  *
