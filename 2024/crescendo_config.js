f762bdd0b36cc14b43ef679d6cac47e336816619
TAIT42 <124452244+TAIT42@users.noreply.github.com>
2024-02-11 22:12:03 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2020lich",
+      "defaultValue": "2022carv",
       "required": "true"
     },
     { "name": "Match Level",
