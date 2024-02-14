4ea2925103b679c38d95873f68506990f2f4868b
TAIT42 <124452244+TAIT42@users.noreply.github.com>
2024-02-14 14:40:57 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -60,7 +60,7 @@ var config_data = `
       "type": "clickable_image",
       "filename": "2024/field_imagereal.png",
       "dimensions": "15 7",
-      "clickRestriction": "5",
+      "clickRestriction": "onePerBox",
       "shape": "circle 5 black red true"
     }
   ],
