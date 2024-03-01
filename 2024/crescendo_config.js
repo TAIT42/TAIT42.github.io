e07dd05480742fc80c1447425f194503c1e666e6
TAIT42 <124452244+TAIT42@users.noreply.github.com>
2024-02-29 21:51:00 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mndu",
+      "defaultValue": "2024mndu",
       "required": "true"
     },
     { "name": "Match Level",
